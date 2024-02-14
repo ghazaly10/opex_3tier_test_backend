ecb663508cb6ed26446a366b9e774bdae3f40daf
Ibrahim Elazb <ibrahim.m.elazb@gmail.com>
2024-02-14 12:02:42 +0200

    commit

--- src/app/departments/new/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import NewDepartmentForm from "../../../components/Departments/NewDepartment";
-async function NewDepartmentPage() {
-  return <NewDepartmentForm  department={null} />;
-}
-
-export default NewDepartmentPage;
